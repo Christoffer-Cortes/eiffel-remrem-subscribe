8171abe7e3b445b46b54b24f1dc3cf9af64d82e0
Daniel Yinanc <daniel.yinanc@ericsson.com>
2016-06-02 17:07:31 -0500

    First Commit

---
+++ src/main/java/com/ericsson/eiffel/remrem/subscribe/util/Util.java
@@ -0,0 +1,20 @@
+package com.ericsson.eiffel.remrem.subscribe.util;
+
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Util {
+    public static Map<String, String> splitQuery(String queryString) throws UnsupportedEncodingException {
+        Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+        String[] pairs = queryString.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder
+                .decode(pair.substring(idx + 1), "UTF-8"));
+        }
+        return query_pairs;
+    }
+}
