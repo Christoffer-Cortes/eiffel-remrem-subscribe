1c53e6976b7e32be7df6683b336434a528391996
hongkailiu <hongkailiu@users.noreply.github.com>
2016-06-06 20:14:48 -0400

    async client

--- src/integration-test/java/com/ericsson/eiffel/remrem/publish/sse/EiffelEmitterIntegrationTest.java
+++ src/integration-test/java/com/ericsson/eiffel/remrem/publish/sse/EiffelEmitterIntegrationTest.java
@@ -3,6 +3,7 @@ package com.ericsson.eiffel.remrem.publish.sse;
 import com.ericsson.eiffel.remrem.publish.sse.helper.RMQHelper;
 import com.ericsson.eiffel.remrem.publish.sse.helper.SSEHelper;
 import com.ericsson.eiffel.remrem.subscribe.App;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +15,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class) @SpringApplicationConfiguration(classes = App.class)
@@ -30,21 +32,27 @@ public class EiffelEmitterIntegrationTest {
         Assert.assertNotNull(sseHelper);
     }
 
+    @After public void tearDown() {
+        sseHelper.stop();
+    }
+
     @Test public void testEiffelSubscriber() throws Exception {
+        final List<String> msgs = new ArrayList<>();
+
         String msg0 = "test-msg-1";
         String msg1 = "test-msg-2";
         String routingKey = "333";
 
-        sseHelper.receive(port, routingKey, 10000);
+        sseHelper.receive(port, routingKey, msgs);
         Thread.sleep(2000);
         rmqHelper.send(routingKey, msg0);
         rmqHelper.send(routingKey, msg1);
         Thread.sleep(8000);
 
-        List<String> msgs = sseHelper.getMsgs();
         Assert.assertEquals(2, msgs.size());
         Assert.assertEquals(msg0, msgs.get(0));
         Assert.assertEquals(msg1, msgs.get(1));
+
     }
 
 }
