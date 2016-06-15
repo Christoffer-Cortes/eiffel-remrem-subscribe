c4f59ffa8321bf461b40d4603c6ddcbaa2e04f04
Hongkai Liu <hongkailiu@users.noreply.github.com>
2016-06-15 15:13:42 -0400

    add javadoc

--- src/main/java/com/ericsson/eiffel/remrem/subscribe/util/ContentHelper.java
+++ src/main/java/com/ericsson/eiffel/remrem/subscribe/util/ContentHelper.java
@@ -4,8 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+/**
+ * Helper class for content conversion
+ */
 @Component("contentHelper") @Slf4j public class ContentHelper {
 
+    /**
+     * get the content which can be correctly handled by most sse clients
+     *
+     * <p>
+     *     remove the line breaks in the given content
+     * </p>
+     * @param content the original content
+     * @return the content where line breaks are replaced by a blank space
+     */
     public String getContent(String content) {
         return StringUtils.replace(StringUtils.replace(content, "\r", " "), "\n", " ");
     }
